efbb22c7d12d7335d519cae2b7bde8264bd48d36
Fabian Jankowski <fjankowsk@gmail.com>
2026-04-18 01:30:15 +0300

    Add script to fix the starting time in chuncked search mode PSRFITS files. This converts the NSUBOFFS to an actual corrected starting time.

---
+++ stools/apps/fix_chunks.py
@@ -0,0 +1,46 @@
+import os.path
+import shutil
+
+from astropy.io import fits
+from astropy.time import Time, TimeDelta
+from astropy import units as u
+
+files = ["merged.fits"]
+SECPERDAY = 24 * 60 * 60
+
+for item in files:
+    print(f"Processing: {item}")
+
+    _root, _extension = os.path.splitext(item)
+    outname = f"{_root}_fixed{_extension}"
+
+    shutil.copy(item, outname)
+
+    with fits.open(outname, mode="update") as hdul:
+        header0 = hdul[0].header
+        _mjd = header0["STT_IMJD"] + (
+            header0["STT_SMJD"] + header0["STT_OFFS"]
+        ) / float(SECPERDAY)
+        start = Time(_mjd, format="mjd")
+        print(start.mjd)
+        print(start.iso)
+
+        header1 = hdul[1].header
+        _offset = header1["NSUBOFFS"] * header1["NSBLK"] * header1["TBIN"]
+        offset = TimeDelta(_offset * u.second)
+        print(offset)
+
+        correct_start = start + offset
+        print(correct_start.mjd)
+        print(correct_start.iso)
+
+        mjd_integer = int((correct_start.jd1 - 2400000.5) + correct_start.jd2)
+        mjd_fraction = (correct_start.jd1 - 2400000.5 + correct_start.jd2) - mjd_integer
+        smjd_integer = int(mjd_fraction * SECPERDAY)
+        smjd_fraction = (mjd_fraction * SECPERDAY) - smjd_integer
+
+        hdul[0].header["STT_IMJD"] = mjd_integer
+        hdul[0].header["STT_SMJD"] = smjd_integer
+        hdul[0].header["STT_OFFS"] = smjd_fraction
+
+        hdul[1].header["NSUBOFFS"] = 0
